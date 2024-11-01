859599e64987f77b86deca8d6de717f350208579
akauzinha47 <kau.vmenezes@gmail.com>
2024-11-01 11:50:02 -0300

    first commit

---
+++ Direction.java
@@ -0,0 +1,11 @@
+public enum Direction {
+    // LEFT, DIAGONAL UPPER LEFT, UP, DIAGONAL UPPER RIGHT, RIGHT, DIAGONAL LOWER RIGHT, DOWN, DIAGONAL LOWER LEFT
+    L,
+    DUL,
+    U,
+    DUR,
+    R,
+    DLR,
+    D,
+    DLL
+}
