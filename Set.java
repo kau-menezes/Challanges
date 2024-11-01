59c633fddfc3d73b0400a67f31fcf37e27f559e6
akauzinha47 <kau.vmenezes@gmail.com>
2024-11-01 11:52:49 -0300

    set

---
+++ Set.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Set {
+    protected ArrayList<Dice> combination;
+
+    public ArrayList<Dice> getCombination() {
+        return combination;
+    }
+
+    public void setCombination(ArrayList<Dice> combination) {
+        this.combination = combination;
+    }
+}
